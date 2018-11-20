49411d670517b7b951274dafbf330e2ff62db5ea
Viacheslav Ronenko <slavic.ronenko@gmail.com>
2018-11-20 17:26:01 +0200

    Feature: Add 404 page

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+export const ROOT_SELECTOR = 'ts-not-found';
+
+@Component({
+  selector: ROOT_SELECTOR,
+  styleUrls: ['./not-found.component.scss'],
+  templateUrl: './not-found.component.html'
+})
+export class NotFoundComponent {}

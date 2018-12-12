5eaf258209ea0b4dcb679daedc884134180002de
Viacheslav Ronenko <slavic.ronenko@gmail.com>
2018-12-12 17:28:48 +0200

    Feature: Add catalogue item component

---
+++ src/app/shared/components/catalogue/catalogue-item/catalogue-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { ICatalogueItem } from '../../../../app.effects';
+
+export const ROOT_SELECTOR = 'ts-catalogue-item';
+
+@Component({
+  selector: ROOT_SELECTOR,
+  styleUrls: ['./catalogue-item.component.scss'],
+  templateUrl: './catalogue-item.component.html'
+})
+export class CatalogueItemComponent {
+  @Input() public item: ICatalogueItem;
+}
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,10 +1,12 @@
 import { CatalogueComponent } from './catalogue/catalogue.component';
+import { CatalogueItemComponent } from './catalogue/catalogue-item/catalogue-item.component';
 import { SpecialOffersComponent } from './special-offers/special-offers.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { SidebarProductComponent } from './sidebar-product/sidebar-product.component';
 
 export default [
   CatalogueComponent,
+  CatalogueItemComponent, // TODO: CatalogueComponent should be a separate module
   SpecialOffersComponent,
   CarouselComponent,
   SidebarProductComponent
